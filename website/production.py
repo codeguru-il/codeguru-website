655cea0f7ed5e5c1eb0e90dd467c4c7f1c944218
Rotem Moses <rotem@faceco.de>
2024-10-26 20:32:34 +0300

    fix: postgres connection string

--- website/production.py
+++ website/production.py
@@ -9,14 +9,14 @@ DEBUG = False
 
 # Postgres connection info
 conn_str = os.getenv("AZURE_POSTGRESQL_CONNECTIONSTRING")
-conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
+conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(';')}
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': conn_str_params['dbname'],
-        'HOST': conn_str_params['host'],
-        'USER': conn_str_params['user'],
-        'PASSWORD': conn_str_params['password'],
+        'NAME': conn_str_params['Database'],
+        'HOST': conn_str_params['Server'],
+        'USER': conn_str_params['User Id'],
+        'PASSWORD': conn_str_params['Password'],
     }
 }
