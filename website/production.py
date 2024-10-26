d82ff24031d3fa7d4ece34260a3327d9d140b1a7
Rotem Moses <rotem@faceco.de>
2024-10-26 20:22:31 +0300

    fix: remove redis cache

--- website/production.py
+++ website/production.py
@@ -7,8 +7,6 @@ ALLOWED_HOSTS = [os.getenv('WEBSITE_HOSTNAME')] if 'WEBSITE_HOSTNAME' in os.envi
 CSRF_TRUSTED_ORIGINS = [f"https://{hostname}" for hostname in ALLOWED_HOSTS]
 DEBUG = False
 
-SESSION_ENGINE = "django.contrib.sessions.backends.cache"
-
 # Postgres connection info
 conn_str = os.getenv("AZURE_POSTGRESQL_CONNECTIONSTRING")
 conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
@@ -20,16 +18,5 @@ DATABASES = {
         'HOST': conn_str_params['host'],
         'USER': conn_str_params['user'],
         'PASSWORD': conn_str_params['password'],
-    }
-}
-
-CACHES = {
-        "default": {
-            "BACKEND": "django_redis.cache.RedisCache",
-            "LOCATION": os.environ.get('AZURE_REDIS_CONNECTIONSTRING'),
-            "OPTIONS": {
-                "CLIENT_CLASS": "django_redis.client.DefaultClient",
-                "COMPRESSOR": "django_redis.compressors.zlib.ZlibCompressor",
-        },
     }
 }
