52d9638673b835fe604e6b6a81821960a8cf3b7b
Rotem Moses <rotem@faceco.de>
2024-10-26 20:47:49 +0300

    fix: postgres connection details

--- website/production.py
+++ website/production.py
@@ -8,15 +8,12 @@ CSRF_TRUSTED_ORIGINS = [f"https://{hostname}" for hostname in ALLOWED_HOSTS]
 DEBUG = False
 
 # Postgres connection info
-conn_str = os.getenv("AZURE_POSTGRESQL_CONNECTIONSTRING")
-conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(';')}
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': conn_str_params['Database'],
-        'HOST': conn_str_params['Server'],
-        'USER': conn_str_params['User Id'],
-        'PASSWORD': conn_str_params['Password'],
+        'NAME': os.getenv('DBNAME'),
+        'HOST': os.getenv('DBHOST'),
+        'USER': os.getenv('DBUSER'),
+        'PASSWORD': os.getenv('DBPASS'),
     }
 }
